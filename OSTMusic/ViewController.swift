87e9c9dee2bd92d3a1e5b64c11421fef54711602
Austin <austingae@gmail.com>
2018-08-06 19:22:19 -0700

    Initial Commit

---
+++ OSTMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSTMusic
+//
+//  Created by austin_gae on 8/6/18.
+//  Copyright © 2018 Austin Gae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
